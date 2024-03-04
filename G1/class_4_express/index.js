c2e176c018c3e4f78382f468882380c424ad566c
Ivo Kostovski <ivo.kostovski@gmail.com>
2024-03-04 20:51:52 +0100

    class 4 done

--- G1/class_4_express/index.js
+++ G1/class_4_express/index.js
@@ -5,14 +5,21 @@ import studentRoutes from './routes/student.route.js';
 const PORT = 3000;
 const HOSTNAME = 'localhost';
 
+// Creating the static page path
 const staticPagePath = path.join(import.meta.dirname, 'public');
 
+// Creating the express server
 const app = express();
 
+app.use(express.json());
+
+// This is a static page
 app.use('/static-page', express.static(staticPagePath));
 
+// This is the main router start-point. /api is commonly used for APIs
 app.use('/api', studentRoutes);
 
+// Starting the express server
 app.listen(PORT, HOSTNAME, () => {
 	console.log(`Server started listening at http://${HOSTNAME}:${PORT}`);
 });
--- G1/class_4_express/routes/student.route.js
+++ G1/class_4_express/routes/student.route.js
@@ -1,20 +1,70 @@
 import { Router } from 'express';
+import fs from 'fs';
+import path from 'path';
+import { pathToFileURL } from 'url';
+import { v4 as uuidv4 } from 'uuid';
 
+const studentsPath = path.join(
+	import.meta.dirname,
+	'..',
+	'data',
+	'students.json'
+);
+
+// Creating a new router which will help us list all the needed routes for students
 const router = Router();
 
 // Get all students
 router.get('/students', (req, res) => {
-	console.log(req);
+	// 1. Get students from the DB (json)
+	const stringifiedStudents = fs.readFileSync(studentsPath);
 
-	const students = [
-		{
-			name: 'Dragan Draganovski',
-			id: 123,
-		},
-	];
+	// 2. parse the students
+	const students = JSON.parse(stringifiedStudents);
 
+	// 3. Send the students to requester
 	res.send(students);
-	// res.sendStatus(200);
 });
+
+// Get one student
+router.get('/students/:id', (req, res) => {
+	// 1. get the id
+	const id = req.params.id;
+
+	// 2. Get all the students
+	const students = JSON.parse(fs.readFileSync(studentsPath));
+
+	// 3. Find the student
+	const student = students.find(s => s.id === id);
+
+	// 4. Send back the student
+	res.send(student);
+});
+
+// Create a new student
+router.post('/students', (req, res) => {
+	const student = req.body;
+
+	const newStudent = {
+		...student,
+		id: uuidv4(),
+	};
+
+	const students = JSON.parse(fs.readFileSync(studentsPath));
+
+	students.push(newStudent);
+
+	const stringifiedStudents = JSON.stringify(students);
+
+	fs.writeFileSync(studentsPath, stringifiedStudents);
+
+	res.status(201).send(newStudent);
+});
+
+// Update a student
+
+// Update a student status
+
+// Delete a student
 
 export default router;
